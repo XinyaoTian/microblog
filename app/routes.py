abd839e635441bf06b016888cee06e3c1e13ce47
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-11-24 11:46:13 +0800

    chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
+# -*- encoding:utf-8 -*-
+# 这个 routes.py 文件主要进行url与相应网页的 map 功能
+
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+def hello():
+    user = {'username':'Daniel'}
+    return render_template('indexSmart.html', user=user)
 
+@app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username':'Winchester'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/loopEg')
+def loopEg():
+    comments_list = [
+        {'author': 'Mike', 'body': 'Good day is today.'},
+        {'author': 'Tim', 'body': 'Beijing is a beautiful city!'}
+    ]
+
+    user = {'username': 'Winchester'}
+
+    return render_template('loopEg.html', user=user, comments=comments_list)
