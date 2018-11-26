053ad4b838dcdd109493f3457263022fa38995ac
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-11-26 09:54:48 +0800

    chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 # -*- encoding:utf-8 -*-
 # 这个 routes.py 文件主要进行url与相应网页的 map 功能
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 
 from app.forms import LoginForm
@@ -44,5 +44,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
