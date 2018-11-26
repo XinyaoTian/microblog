27ec125b314f2b27a907bb1326e027625058d3ef
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-11-26 15:02:31 +0800

    post table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 # moduel of user table
@@ -6,7 +7,23 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # Relation to Post moduel
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # The __repr__ method tells Python how to print objects of this class
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+# moduel of post table
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(150))
+    # timestamp arg default passing the function itself, not the result.
+    # mention the utcnow
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # mention the usage of ForeigneKey
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
