46746763f821ebff9228d868959553f150af5636
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-11-26 13:21:31 +0800

    chapter4 db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
 # Add secert-key
 app.config.from_object(Config)
 
+# db object represents the database
+db = SQLAlchemy(app)
+
+# migrate object represents the migration engine
+migrate = Migrate(app, db)
+
 from app import routes
+
+# module will define the structure of the database
+from app import models
