ba63d070038c14020099a5ca2f27b684ebe53f05
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-12-03 14:17:12 +0800

    chapter 8

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+# for sending email
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
-# Add secert-key and other configurations
+# Add secret-key and other configurations
 app.config.from_object(Config)
 
 # db object represents the database
@@ -21,7 +25,39 @@ login = LoginManager(app)
 # force login or the user will not view other pages
 login.login_view = 'login' # login url
 
+# Using SMTP to send email
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                credentials=auth, secure=secure
+            )
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+    # Logging file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
 from app import routes
 
 # module will define the structure of the database
 from app import models
+
+# for 404 and 500 errors handling
+from app import errors
--- app/models.py
+++ app/models.py
@@ -9,19 +9,21 @@ from flask_login import UserMixin
 # for user avatar
 from hashlib import md5
 
-# return unique user id
-# The user loader is registered with Flask-Login with the @login.user_loader decorator.
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+# a many-to-many relation
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
-# moduel of user table
+
+# module of user table
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    # Relation to Post moduel
+    # Relation to Post module
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # New function of self-introduction
@@ -32,6 +34,17 @@ class User(UserMixin, db.Model):
     # $ flask db migrate -m "some comments..."
     # $ flask db upgrade
 
+    # auxiliary table
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    # The __repr__ method tells Python how to print objects of this class
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
     # The whole password hashing logic can be implemented as two new methods in the user model:
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -40,15 +53,39 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    # The __repr__ method tells Python how to print objects of this class
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
     # Generate avatar
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    # functions of following
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        # Note how the followed and own queries are combined into one
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
+# return unique user id
+# The user loader is registered with Flask-Login with the @login.user_loader decorator.
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 # moduel of post table
@@ -63,3 +100,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
