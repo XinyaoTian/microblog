9393d654d86a78f2adc22929d887b6b512e2d205
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-12-03 15:10:27 +0800

    chapter 8

--- app/routes.py
+++ app/routes.py
@@ -42,10 +42,12 @@ def before_request():
         db.session.add(current_user) # which is not necessary cause current user is already there
         db.session.commit()
 
+
 @app.route('/index')
 def index():
     user = {'username':'Winchester'}
     return render_template('index.html', title='Home', user=user)
+
 
 @app.route('/loopEg')
 def loopEg():
@@ -57,6 +59,7 @@ def loopEg():
     user = {'username': 'Winchester'}
 
     return render_template('loopEg.html', user=user, comments=comments_list)
+
 
 # Never Use Capital Letters in URLs ! No 'welcomePage' !
 @app.route('/')
@@ -75,6 +78,7 @@ def welcome():
     ]
 
     return render_template('welcomePage.html', title='Home Page', posts=posts)
+
 
 # a fake login function
 # @app.route('/login', methods=['GET', 'POST'])
@@ -106,6 +110,7 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -120,11 +125,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 # logout
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('welcome'))
+
 
 @app.route('/user/<username>')
 @login_required
@@ -135,6 +142,7 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -152,3 +160,35 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('welcome'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('welcome'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
