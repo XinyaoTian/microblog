7b80253e1dc46bd9515b66e675114f52091a3859
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-12-04 11:50:53 +0800

    chapter 9

--- app/routes.py
+++ app/routes.py
@@ -91,10 +91,16 @@ def welcome():
     # posts = current_user.followed_posts().all()
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    # Go to next page or previous page
+    next_url = url_for('welcome', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('welcome', page=posts.prev_num) \
+        if posts.has_prev else None
 
     # The template receives the form object as an additional argument
     return render_template('welcomePage.html', title='Home Page',
-                           form=form, posts=posts.items)
+                           form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 # a fake login function
@@ -222,4 +228,9 @@ def explore():
     # posts = Post.query.order_by(Post.timestamp.desc()).all()
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('welcomePage.html', title='Explore', posts=posts.items)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('welcomePage.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
